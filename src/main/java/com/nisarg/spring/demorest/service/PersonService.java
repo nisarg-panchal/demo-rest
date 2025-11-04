56e9c0afd1d08a78888e91a2518e0fb83e5bac8e
Nisarg Panchal <nisarg.panchaal@gmail.com>
2025-11-04 14:45:58 +1000

    MDC added

--- src/main/java/com/nisarg/spring/demorest/service/PersonService.java
+++ src/main/java/com/nisarg/spring/demorest/service/PersonService.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
@@ -23,22 +24,28 @@ public class PersonService {
 
   @CachePut(value = "persons", key = "#result.id")
   public PersonDto save(PersonDto personDto) {
+    MDC.put("SavePerson", personDto.getName() + "-" + personDto.getEmail());
     var person = personRepository.save(personMapper.toEntity(personDto));
     log.info("Saved person with id: {}", person.getId());
+    MDC.clear();
     return personMapper.toDto(person);
   }
 
   @CachePut(value = "persons", key = "#id")
   public PersonDto findById(UUID id) {
+    MDC.put("id", id.toString());
     var person = personRepository.findById(id);
     log.info("Found person with id: {}", id);
+    MDC.clear();
     return person.map(personMapper::toDto).orElse(null);
   }
 
   @CachePut(value = "persons")
   public List<PersonDto> findAll() {
+    MDC.put("findAll", UUID.randomUUID().toString());
     var persons = personRepository.findAll();
     log.info("Found {} persons", persons.size());
+    MDC.clear();
     return persons.stream()
         .map(personMapper::toDto)
         .toList();
@@ -46,8 +53,10 @@ public class PersonService {
 
   @CacheEvict(value = "persons", key = "#id")
   public void deleteById(UUID id) {
+    MDC.put("id", id.toString());
     log.info("Deleting person with id: {}", id);
     personRepository.deleteById(id);
+    MDC.clear();
   }
 
   @CacheEvict(value = "persons", allEntries = true)
@@ -73,9 +82,11 @@ public class PersonService {
       log.warn("Person with id {} not found for update", id);
       return null;
     }
+    MDC.put("updatePerson", personDto.getName() + "-" + personDto.getEmail());
     personDto.setId(id);
     var updatedPerson = personRepository.save(personMapper.toEntity(personDto));
     log.info("Updated person with id: {}", id);
+    MDC.clear();
     return personMapper.toDto(updatedPerson);
   }
 }
