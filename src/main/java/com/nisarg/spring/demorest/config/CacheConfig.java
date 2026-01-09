4fbacc3357c131a4e482e44251674485f707e414
Nisarg Panchal <nisarg.panchaal@gmail.com>
2026-01-09 15:15:17 +1000

    Upgraded to spring-boot 4.0.1

---
+++ src/main/java/com/nisarg/spring/demorest/config/CacheConfig.java
@@ -0,0 +1,18 @@
+package com.nisarg.spring.demorest.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+  @Bean(name = "cacheManager")
+  public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+    return RedisCacheManager.builder(connectionFactory).build();
+  }
+}
